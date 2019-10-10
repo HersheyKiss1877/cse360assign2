e92e3ff0e5093a77c7370149b49c3e1b688cc806
Ian <Ian@asu.edu>
2019-10-09 18:45:13 -0700

    Complete code for AddingMachine class

--- 360/src/cse360assign2/AddingMachine.java
+++ 360/src/cse360assign2/AddingMachine.java
@@ -11,51 +11,62 @@ public class AddingMachine{
 	private int total;
 	
 	/**
+	 * Holds the order of operations and values passed into the calculator
+	 */
+	private String calchistory;
+	
+	/**
 	 * creates a new AddingMachine with the final value initialized to 0
 	 */
 	public AddingMachine(){
 		total = 0; //not needed - included for clarity
+		calchistory = "0";
 	}
 	
 	/**
 	 * gets the total value after all operations
-	 * @return total
+	 * @return total - this is the running total of all calculations
 	 */
 	public int getTotal(){
-		return 0;
+		return total;
 	}
 	
 	/**
 	 * adds some value to the running total
+	 * adds to the record of calculations
 	 * @param value - value to be added to the running total
 	 */
 	public void add(int value){
-		
+		total += value;
+		calchistory += " + " + value;
 	}
 	
 	/**
 	 * subtracts some value from the running total
+	 * adds to the record of calculations
 	 * @param value - value to be subtracted from the running total
 	 */
 	public void subtract(int value) {
-		
+		total -= value;
+		calchistory += " - " + value;
 	}
 	
 	/**
 	 * organizes the operations and numbers used in the calculations
 	 * of the running total
-	 * @return "" - this will contain the operations and values in order
+	 * @return calchistory - this will contain the operations and values in order
 	 * 				for the calculations to the running total
 	 */
 	public String toString() {
-		return "";
+		return calchistory;
 	}
 	
 	/**
-	 * resets all variables within the class
+	 * resets all variables within the class to there initial values
 	 */
 	public void clear() {
-		
+		total = 0;
+		calchistory = "0";
 	}
 	
 }
