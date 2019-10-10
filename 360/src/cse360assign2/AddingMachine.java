9a27b7ca7842d7ba78fda9c4a92dcd0a0fb8e27d
Ian <Ian@Hershey-Kiss>
2019-10-09 18:31:42 -0700

    Initial push with javadoc comments

--- 360/src/cse360assign2/AddingMachine.java
+++ 360/src/cse360assign2/AddingMachine.java
@@ -1,28 +1,59 @@
 package cse360assign2;
 
+/**
+ * Represents a calculator that can add and subtract numbers
+ * then prints the final value
+ */
 public class AddingMachine{
+	/**
+	 * Holds the final value for the operations done with the calculator
+	 */
 	private int total;
 	
+	/**
+	 * creates a new AddingMachine with the final value initialized to 0
+	 */
 	public AddingMachine(){
 		total = 0; //not needed - included for clarity
 	}
 	
+	/**
+	 * gets the total value after all operations
+	 * @return total
+	 */
 	public int getTotal(){
 		return 0;
 	}
 	
+	/**
+	 * adds some value to the running total
+	 * @param value - value to be added to the running total
+	 */
 	public void add(int value){
 		
 	}
 	
+	/**
+	 * subtracts some value from the running total
+	 * @param value - value to be subtracted from the running total
+	 */
 	public void subtract(int value) {
 		
 	}
 	
+	/**
+	 * organizes the operations and numbers used in the calculations
+	 * of the running total
+	 * @return "" - this will contain the operations and values in order
+	 * 				for the calculations to the running total
+	 */
 	public String toString() {
 		return "";
 	}
 	
+	/**
+	 * resets all variables within the class
+	 */
 	public void clear() {
 		
 	}
